258f7e059904e45e3aa803859dc57834b7672648
robmllze <86869208+robmllze@users.noreply.github.com>
2025-02-13 01:46:07 +1100

    update

--- lib/src/safe_sequential.dart
+++ lib/src/safe_sequential.dart
@@ -70,13 +70,13 @@ class SafeSequential {
     Duration? buffer,
   }) {
     ResolvableOption<T> fn(ResultOption previous) => Resolvable.unsafe(() {
-      final temp = unsafe(previous);
-      if (temp is Future<Option<T>?>) {
-        return temp.then((e) => e ?? const None());
-      } else {
-        return temp ?? const None();
-      }
-    });
+          final temp = unsafe(previous);
+          if (temp is Future<Option<T>?>) {
+            return temp.then((e) => e ?? const None());
+          } else {
+            return temp ?? const None();
+          }
+        });
     return addSafe<T>(fn, buffer: buffer);
   }
 
@@ -109,18 +109,16 @@ class SafeSequential {
     // ignore: invalid_use_of_visible_for_testing_member
     final value = _current.value;
     if (value is Future<Result<Option<Object>>>) {
-      _current =
-          Async.unsafe(() async {
-            final temp = function(await value);
-            if (temp == null) {
-              return _current;
-            }
-            _isEmpty = true;
-            return temp;
-          }).merge();
+      _current = Async.unsafe(() async {
+        final temp = function(await value);
+        if (temp == null) {
+          return _current;
+        }
+        _isEmpty = true;
+        return temp;
+      }).merge();
     } else {
-      _current =
-          function(value)?.map((e) {
+      _current = function(value)?.map((e) {
             _isEmpty = true;
             return e;
           }) ??
@@ -133,7 +131,7 @@ class SafeSequential {
   Resolvable<void> get last => add(unsafe: (_) => null);
 }
 
-typedef TFutureOrOption<T extends Object> = FutureOr<Option<T>?>;
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
-typedef TAddFunction<T extends Object> =
-    TFutureOrOption<T> Function(ResultOption previous);
+typedef TFutureOrOption<T extends Object> = FutureOr<Option<T>?>;
+typedef TAddFunction<T extends Object> = TFutureOrOption<T> Function(ResultOption previous);
