ea6ca090c2339905f57423df8046de78a2c99d8b
robmllze <86869208+robmllze@users.noreply.github.com>
2025-06-21 00:53:18 -0400

    +Remove confusing consts and typedefs

--- lib/src/utils/typedefs.dart
+++ lib/src/utils/typedefs.dart
@@ -16,28 +16,16 @@ import '/src/_src.g.dart';
 
 // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
-const NONE = None<Never>();
-
-const SYNC_NONE = TSyncNone.unsafe(Ok(None<Never>()));
-
-const TResolvableNone<Never> RESOLVABLE_NONE = SYNC_NONE;
+const NONE = None<Unit>();
 
 @pragma('vm:prefer-inline')
-Sync<None<T>> syncNone<T extends Object>() =>
-    const TSyncNone.unsafe(Ok(None()));
+Sync<None<T>> syncNone<T extends Object>() => const Sync.unsafe(Ok(None()));
 
 @pragma('vm:prefer-inline')
-Async<None<T>> asyncNone<T extends Object>() =>
-    TAsyncNone.unsafe(Future.value(Ok(None<T>())));
+Async<None<T>> asyncNone<T extends Object>() => Async.unsafe(Future.value(Ok(None<T>())));
 
 @pragma('vm:prefer-inline')
 Resolvable<None<T>> resolvableNone<T extends Object>() => syncNone();
 
 typedef TReduced<T extends Object> = Resolvable<Option<T>>;
 typedef TOptionResult<T extends Object> = Option<Result<T>>;
-typedef TResolvableNone<T extends Object> = Resolvable<None<T>>;
-typedef TSyncNone<T extends Object> = Sync<None<T>>;
-typedef TAsyncNone<T extends Object> = Async<None<T>>;
-typedef TResolvableSome<T extends Object> = Resolvable<Some<T>>;
-typedef TSyncSome<T extends Object> = Sync<Some<T>>;
-typedef TAsyncSome<T extends Object> = Async<Some<T>>;
